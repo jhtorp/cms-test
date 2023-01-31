7af5db7367b700867cbd937fc6c58e333a756c6c
Jørund Torp <jorundtorp@Jrunds-MacBook-Air.local>
2023-01-31 16:57:46 +0100

    Second commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,45 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+
+</script>
+
+
+<main>
+
+    <div class="wrapper">
+        <h1>Åpningstider</h1>
+        <div class="container">
+            <div class="dag man">Mandag: 10:00-16:00</div>
+            <div class="dag tir">Tirsdag: 10:00-16:00</div>
+            <div class="dag ons">Onsdag: 10:00-16:00</div>
+            <div class="dag tor">Torsdag: 10:00-16:00</div>
+            <div class="dag fre">Fredag: 10:00-16:00</div>
+        </div>
+    </div>
+
+</main>
+
+
+<style>
+    main {
+        display: grid;
+        place-content: center;
+        height: 100vh;
+        width: 100vw;
+    }
+    p {
+        text-align: center;
+        font-size: larger;
+    }
+
+    .container {
+        display: flex;
+        flex-direction: column;
+        gap: 10px;
+    }
+
+    .dag {
+        width: 100%;
+        font-size: 20px;
+
+    }
+</style>
